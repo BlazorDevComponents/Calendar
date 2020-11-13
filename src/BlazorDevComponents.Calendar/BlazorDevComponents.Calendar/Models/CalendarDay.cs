20a701e6f52bf324b37c8feb49a0923ad2a5b37e
Carl Randall <carlrandall@live.com>
2020-11-13 16:16:40 +0000

    basic calendar with supporting model

---
+++ src/BlazorDevComponents.Calendar/BlazorDevComponents.Calendar/Models/CalendarDay.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorDevComponents.Calendar.Models
+{
+    public class CalendarDay
+    {
+        public int DayNumber { get; set; }
+        public DateTime Date { get; set; }        
+    }
+}
